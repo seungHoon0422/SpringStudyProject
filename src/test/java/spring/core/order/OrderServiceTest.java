acdada4d48bcadb084783b06b02a0265b7c92b15
BaekSeungHoon <psh104404@naver.com>
2022-11-27 20:55:28 +0900

    add order, discount junit test

---
+++ src/test/java/spring/core/order/OrderServiceTest.java
@@ -0,0 +1,28 @@
+package spring.core.order;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import spring.core.member.Grade;
+import spring.core.member.Member;
+import spring.core.member.MemberService;
+import spring.core.member.MemberServiceImpl;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member memberA = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(memberA);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+
+
+    }
+
+
+}
